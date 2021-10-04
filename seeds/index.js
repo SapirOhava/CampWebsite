4e5c040285a018b091c5c37368ef9e79da56c95b
sapir ohava <sapirlove24@gmail.com>
2021-10-04 12:24:14 +0300

    setting basic authorization

--- seeds/index.js
+++ seeds/index.js
@@ -18,6 +18,7 @@ const seedDB = async () => {
         const price = Math.floor(Math.random()*20)+10;
 
         const camp = new Campground({
+            author:'615982840b2826b1eca6c915',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/155011',
